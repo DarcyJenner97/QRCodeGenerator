4401204bd48c264c6cdfde8f999d81961e3c0d11
Darcy Jenner <gigledy@gmail.com>
2024-09-14 16:22:44 +0930

    Added download button, error message for not typing and made downloads work

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
+// Instanciate the QR Code
 var qrcode = new QRCode("qrcode");
 
 
-
+// Generate the QR code 
 function generateQR(){
     let text = document.getElementById("qrInput").value;
     if (!text){
@@ -10,4 +11,30 @@ function generateQR(){
     }
     document.getElementById("noTextError").style.visibility = "hidden";
     qrcode.makeCode(text)
+    remove = document.getElementById("downloadButton")
+    if(!remove){
+        createButton();
+    }
+}
+
+// create the download button
+function createButton(){
+    let button = document.createElement("BUTTON");
+    button.id = "downloadButton"
+    button.innerHTML = "Download QR Code"
+    button.addEventListener("click", downloadQRCode);
+    let qr = document.getElementById("qrcode");
+    qr.appendChild(button);
+}
+
+// Downloads the created QR Code
+function downloadQRCode(){
+    let qrImage = document.querySelector("#qrcode img");
+    if (qrImage) {
+        let link = document.createElement('a');
+        link.href = qrImage.src;
+        let downloadName = document.getElementById("qrcode").title;
+        link.download = `${downloadName} QRCode.png`; // Set the name for the downloaded file
+        link.click();
+    }
 }
